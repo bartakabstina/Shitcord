9bcaec4d37cfb60ad2415bf8e5a373ce5b384af7
bartakabstina <47223145+bartakabstina@users.noreply.github.com>
2019-10-11 23:05:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 // Configuration
 const prefix = "$";
-const owner_ids = ["492975557445943296"];
+const owner_ids = ["218668172004294657"];
 
 function isOwner(id) {
     for(var i of owner_ids) {
